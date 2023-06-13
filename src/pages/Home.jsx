e82a58408d875edc5f11cfc7b1c1db08a3ccec87
diegoram23 <diego.pta.oc@gmail.com>
2023-06-12 21:15:07 -0700

    hero image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import BpForm from "../components/BpForm";
 import RecentReadings from "../components/RecentReadings";
 import useFetch from "../components/useFetch";
+import BpBackground from '../bp-background.jpg'
 
 const Home = () => {
-    const {dataReadings} = useFetch()
+    const { dataReadings } = useFetch()
 
     return (
-        <>
+        <div>
+            <section>
+               <img className="hero-img" src={BpBackground}/>
+            </section>
             <BpForm />
             <RecentReadings />
-        </>
+        </div>
     );
 }
 
